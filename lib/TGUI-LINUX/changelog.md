dd5241d1790ade6f01ca28c7a09552e357483572
mpolec <polec@nomachine.com>
2021-09-04 16:38:00 +0200

    Updated build for Linux devices

---
+++ lib/TGUI-LINUX/changelog.md
@@ -0,0 +1,67 @@
+TGUI 0.9.1  (12 February 2021)
+------------------------------
+
+- Added new SpinControl widget (combination of EditBox and SpinButton) ([PR #135](https://github.com/texus/TGUI/pull/135))
+- Added new TabContainer widget (combination of Tabs with Panel below) ([PR #139](https://github.com/texus/TGUI/pull/139))
+- Holding down arrow on SpinButton will now keep chaning the value ([PR #137](https://github.com/texus/TGUI/pull/137))
+- Added insertItem function to ListView ([PR #138](https://github.com/texus/TGUI/pull/138))
+- Up and down arrows now change selected item in ListBox and ListView ([PR #146](https://github.com/texus/TGUI/pull/146))
+- Horizontal scrollbar can now depend on item width in ListView ([PR #147](https://github.com/texus/TGUI/pull/147))
+- Added support to copy selected ListView items to clipboard ([PR #148](https://github.com/texus/TGUI/pull/148))
+- Add setTextSize for SubwidgetContainer ([PR #149](https://github.com/texus/TGUI/pull/149))
+- Rewrote signal system again, `b->connect("Pressed",...)` is now `b->onPress(...)`
+- Added new FileDialog widget
+- Added new ToggleButton widget
+- Added new SeparatorLine widget
+- Added support for setting mouse cursor + use them on resizable child windows
+- Added timers and optional gui.mainLoop()
+- Added experimental setOrigin, setScale and setRotation functions to Widget
+- Added ThumbWithinTrack to Slider renderer to have thumb align with track on sides
+- Added DoubleClick signal to Panel
+- Added ViewChanged signal to GuiBase
+- Added hover and selected border colors for Tabs
+- Added TextureSelectedTrack property to RangeSlider renderer
+- Added String::fromNumberRounded to convert float to string with a fixed amount of decimals
+- Added startsWith and endsWith helper functions to String
+- Added RoundedBorderRadius property to button renderer
+- Separators can be added to MenuBar by inserting menu items with "-" string
+- ListBox and ListView can now store user data in their items
+- ListView icons can be given a fixed size to rescale all icons to requested size
+- Replaced all std::string and sf::String by tgui::String
+- Replaced Text, Color, Rect and Vector2 classes from SFML with own versions
+- Replaced sf::Text::Style with tgui::TextStyle
+- Replaced setView in Gui by view/viewport setters with absolute or relative values
+- Added onClosing signal to ChildWindow to have a better way to abort closing it
+- Signal names, renderer names and widget/renderer properties are now case-sensitive
+- Added setWidth and setHeight helper functions
+- Added getCheckedRadioButton to RadioButtonGroup
+- Textures are now smooth by default
+- Size of ChildWindow now includes borders and title bar
+- When setView is not called on Gui, view will now scale with window size
+- Menus of menu bar are now always on top of all other widgets
+- Swapped padding and alignment parameters of addWidget in Grid
+- Moved uncheckRadioButtons from Container to RadioButtonGroup
+- Theme::setDefault now takes a shared_ptr or a filename as parameter
+- Changed return type of addItem in ListBox/ComboBox to return index of the item
+- Changed Knob value type from int to float
+- Changed default value of ChangeItemOnScroll in ComboBox to false
+- Changed default value of ExpandDirection in ComboBox to Automatic
+- Changed default value of ScrollbarPolicy in Label to Automatic
+- Renamed TextBox to TextArea
+- Renamed getMenuList in MenuBar to getMenus
+- Renamed mouseOnWidget to isMouseOnWidget
+- Renamed TextStyle class to TextStyles and TextStyles::Style enum to TextStyle
+- Default scroll amount in ScrollablePanel now depends on global text size
+- Gui::getFont now returns the global font if no font was set in the Gui
+- Container now translates the widget position before calling draw function
+- Dragging scrollbar inside child window didn't work when mouse left child window
+- Selected part of RangeSlider wasn't drawn when using textures
+- Removed all code that was marked as deprecated
+- Binding left and top in layouts now works correctly when the origin is changed
+- Some other small changes that weren't added to the changelog
+
+Older Releases
+--------------
+
+See the website for changelogs of older releases: https://tgui.eu/changelog/
+
---
+++ lib/TGUI-LINUX/tests/CompareFiles.cpp
@@ -0,0 +1,148 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// TGUI - Texus' Graphical User Interface
+// Copyright (C) 2012-2021 Bruno Van de Velde (vdv_b@tgui.eu)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "Tests.hpp"
+#include <fstream>
+#include <cstring>
+
+// Rendering isn't identical on different computers, so we just check that the image looks similar enough.
+// The worst encountered difference was 1.43% on Label_Simple.png, so errors less than 1.5% of the entire
+// image are allowed. Since 100% would mean going from a completely black to completely white image, the
+// percentage is high enough to make minor errors go unnoticed, but it is better than not checking at all.
+// Worse test on Github Actions: Clipping_NestedLayers.png at 1.71%
+void compareImageFiles(const tgui::String& filename1, const tgui::String& filename2)
+{
+    sf::Image image1;
+    if (!image1.loadFromFile(filename1.toStdString()))
+        REQUIRE(image1.loadFromFile(filename1.toStdString()));
+
+    sf::Image image2;
+    if (!image2.loadFromFile(filename2.toStdString()))
+        REQUIRE(image2.loadFromFile(filename2.toStdString()));
+
+    if (image1.getSize() != image2.getSize())
+        REQUIRE(image1.getSize() == image2.getSize());
+
+    double totalDiff = 0;
+    const sf::Uint8* pixels1 = image1.getPixelsPtr();
+    const sf::Uint8* pixels2 = image2.getPixelsPtr();
+    for (unsigned int y = 0; y < image1.getSize().y; ++y)
+    {
+        for (unsigned int x = 0; x < image1.getSize().x; ++x)
+        {
+            unsigned int index = ((y * x) + x) * 4;
+            totalDiff += std::abs(int(pixels1[index+0]) - int(pixels2[index+0])) / 255.0;
+            totalDiff += std::abs(int(pixels1[index+1]) - int(pixels2[index+1])) / 255.0;
+            totalDiff += std::abs(int(pixels1[index+2]) - int(pixels2[index+2])) / 255.0;
+        }
+    }
+
+    double diffPercentage = (totalDiff * 100)  / (image1.getSize().x * image1.getSize().y * 3);
+    REQUIRE(diffPercentage < 1.75);
+}
+
+bool compareFiles(const tgui::String& leftFileName, const tgui::String& rightFileName)
+{
+    std::ifstream leftFile;
+    std::ifstream rightFile;
+    char leftRead = 0;
+    char rightRead = 0;
+    bool result;
+
+    // Open the two files.
+    leftFile.open(leftFileName.toStdString().c_str(), std::ios_base::in | std::ios_base::binary);
+    if (!leftFile.is_open())
+        return false;
+
+    rightFile.open(rightFileName.toStdString().c_str(), std::ios_base::in | std::ios_base::binary);
+    if (!rightFile.is_open())
+    {
+        leftFile.close();
+        return false;
+    }
+
+    result = true; // Files exist and are open, assuming equality unless a counterexamples shows up
+    while (result && leftFile.good() && rightFile.good())
+    {
+        leftFile.get(leftRead);
+        rightFile.get(rightRead);
+        result = (leftRead == rightRead);
+    }
+
+    if (result)
+    {
+        // Last read was still equal, are we at the end of both files?
+        result = (!leftFile.good()) && (!rightFile.good());
+    }
+
+    leftFile.close();
+    rightFile.close();
+    return result;
+}
+
+// Tests the file comparison function
+TEST_CASE("compareFiles")
+{
+    std::ofstream myfile;
+    myfile.open("file1.txt");
+    myfile.close();
+    myfile.open("file2.txt");
+    myfile.close();
+
+    // Compare 2 empty files
+    REQUIRE(compareFiles("file1.txt", "file2.txt"));
+    REQUIRE(compareFiles("file2.txt", "file1.txt"));
+
+    // Compare an empty and a non-empty files
+    myfile.open("file1.txt");
+    myfile << "xxx" << std::endl << "yyy";
+    myfile.close();
+    REQUIRE(!compareFiles("file1.txt", "file2.txt"));
+    REQUIRE(!compareFiles("file2.txt", "file1.txt"));
+
+    // Compare two equal files
+    myfile.open("file2.txt");
+    myfile << "xxx" << std::endl << "yyy";
+    myfile.close();
+    REQUIRE(compareFiles("file1.txt", "file2.txt"));
+    REQUIRE(compareFiles("file2.txt", "file1.txt"));
+
+    // Compare 2 non-empty files which are off by a character in the middle
+    myfile.open("file2.txt");
+    myfile << "xxx" << std::endl << "xyy";
+    myfile.close();
+    REQUIRE(!compareFiles("file1.txt", "file2.txt"));
+    REQUIRE(!compareFiles("file2.txt", "file1.txt"));
+
+    // Compare 2 non-empty files where one is one character shorter than the other
+    myfile.open("file2.txt");
+    myfile << "xxx" << std::endl << "yy";
+    myfile.close();
+    REQUIRE(!compareFiles("file1.txt", "file2.txt"));
+    REQUIRE(!compareFiles("file2.txt", "file1.txt"));
+
+    // Compare existig against non existing file
+    REQUIRE(!compareFiles("file1.txt", "nonexisting.txt"));
+    REQUIRE(!compareFiles("nonexisting.txt", "file1.txt"));
+}
