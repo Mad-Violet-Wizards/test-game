1cb85e7fc0cd6f0e3b5e031cfbe5b991b1960f7f
HoopDead <polec0@wp.pl>
2022-02-21 22:36:47 +0100

    Added conan as a package manager.
    * Added compilation script for Windows,
    * Removed lib folder,
    * Renamed button names in SceneMenu, removed horizontal layout,
    * Setup conan as a main package manager.

--- src/Game/src/Scene/SceneMenu.cpp
+++ src/Game/src/Scene/SceneMenu.cpp
@@ -8,70 +8,37 @@ SceneMenu::SceneMenu(Window* window) : m_window(window)
 {
   m_menuLayout = std::make_shared<GuiLayout>(m_window);
   m_testVerticalLayout = std::make_shared<GuiLayoutVertical>(m_window);
-  m_test = std::make_shared<GuiTextWidget>(m_window);
-  m_test -> SetColor(sf::Color::White);
-  m_test -> SetCharacterSize(24);
-  m_test -> SetText("New Game");
+  m_buttonNewGame = std::make_shared<GuiTextWidget>(m_window);
+  m_buttonNewGame -> SetColor(sf::Color::White);
+  m_buttonNewGame -> SetCharacterSize(24);
+  m_buttonNewGame -> SetText("New Game");
 
-  m_test1 = std::make_shared<GuiTextWidget>(m_window);
-  m_test1 -> SetColor(sf::Color::White);
-  m_test1 -> SetCharacterSize(24);
-  m_test1 -> SetText("Load Game");
-  m_test1 -> SetAlign(GuiObject::GuiAlign::AlignCenter);
+  m_buttonLoadGame = std::make_shared<GuiTextWidget>(m_window);
+  m_buttonLoadGame -> SetColor(sf::Color::White);
+  m_buttonLoadGame -> SetCharacterSize(24);
+  m_buttonLoadGame -> SetText("Load Game");
+  m_buttonLoadGame -> SetAlign(GuiObject::GuiAlign::AlignCenter);
 
-  m_test2 = std::make_shared<GuiTextWidget>(m_window);
-  m_test2 -> SetColor(sf::Color::White);
-  m_test2 -> SetCharacterSize(24);
-  m_test2 -> SetText("Options");
-  m_test2 -> SetAlign(GuiObject::GuiAlign::AlignCenter);
+  m_buttonOptions = std::make_shared<GuiTextWidget>(m_window);
+  m_buttonOptions -> SetColor(sf::Color::White);
+  m_buttonOptions -> SetCharacterSize(24);
+  m_buttonOptions -> SetText("Options");
+  m_buttonOptions -> SetAlign(GuiObject::GuiAlign::AlignCenter);
 
-  m_test3 = std::make_shared<GuiTextWidget>(m_window);
-  m_test3 -> SetColor(sf::Color::White);
-  m_test3 -> SetCharacterSize(24);
-  m_test3 -> SetText("Exit");
-  m_test3 -> SetAlign(GuiObject::GuiAlign::AlignCenter);
+  m_buttonExit = std::make_shared<GuiTextWidget>(m_window);
+  m_buttonExit -> SetColor(sf::Color::White);
+  m_buttonExit -> SetCharacterSize(24);
+  m_buttonExit -> SetText("Exit");
+  m_buttonExit -> SetAlign(GuiObject::GuiAlign::AlignCenter);
 
   m_testVerticalLayout->SetRelativeSize(100, 0);
   m_testVerticalLayout->SetRelativePosition(0, 25);
-  m_testVerticalLayout->AddWidget(m_test);
-  m_testVerticalLayout->AddWidget(m_test1);
-  m_testVerticalLayout->AddWidget(m_test2);
-  m_testVerticalLayout->AddWidget(m_test3);
-
-  m_testHorizontalLayout = std::make_shared<GuiLayoutHorizontal>(m_window);
-  m_testH = std::make_shared<GuiTextWidget>(m_window);
-  m_testH -> SetColor(sf::Color::White);
-  m_testH -> SetCharacterSize(24);
-  m_testH -> SetText("Exit");
-  m_testH -> SetAlign(GuiObject::GuiAlign::AlignTop);
-
-  m_testH1 = std::make_shared<GuiTextWidget>(m_window);
-  m_testH1 -> SetColor(sf::Color::White);
-  m_testH1 -> SetCharacterSize(24);
-  m_testH1 -> SetText("Load Game");
-  m_testH1 -> SetAlign(GuiObject::GuiAlign::AlignCenter);
-
-  m_testH2 = std::make_shared<GuiTextWidget>(m_window);
-  m_testH2 -> SetColor(sf::Color::White);
-  m_testH2 -> SetCharacterSize(24);
-  m_testH2 -> SetText("Options");
-  m_testH2 -> SetAlign(GuiObject::GuiAlign::AlignBottom);
-
-  m_testH3 = std::make_shared<GuiTextWidget>(m_window);
-  m_testH3 -> SetColor(sf::Color::White);
-  m_testH3 -> SetCharacterSize(24);
-  m_testH3 -> SetText("New Game");
-  m_testH3 -> SetAlign(GuiObject::GuiAlign::AlignTop);
-
-  m_testHorizontalLayout -> SetRelativeSize(100, 25);
-  m_testHorizontalLayout -> SetRelativePosition(0, 60);
-  m_testHorizontalLayout->AddWidget(m_testH);
-  m_testHorizontalLayout->AddWidget(m_testH1);
-  m_testHorizontalLayout->AddWidget(m_testH2);
-  m_testHorizontalLayout->AddWidget(m_testH3);
+  m_testVerticalLayout->AddWidget(m_buttonNewGame);
+  m_testVerticalLayout->AddWidget(m_buttonLoadGame);
+  m_testVerticalLayout->AddWidget(m_buttonOptions);
+  m_testVerticalLayout->AddWidget(m_buttonExit);
 
   m_menuGuiManager.AddLayout(GuiManager::LayoutLevel::Level1, m_testVerticalLayout);
-  m_menuGuiManager.AddLayout(GuiManager::LayoutLevel::Level2, m_testHorizontalLayout);
 }
 
 SceneMenu::~SceneMenu() {}
