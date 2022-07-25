1d1e1c8e0784ab04c4e9e9503781617591a3e6a1
HoopDead <polec0@wp.pl>
2022-07-25 23:48:00 +0200

    Fixed indent in GuiImage, fixed compilation due to removal of GuiImage test from SceneMenu

--- src/game/src/SceneMenu.cpp
+++ src/game/src/SceneMenu.cpp
@@ -106,7 +106,6 @@ void SceneMenu::CreateGUI()
   m_mainMenuLayout -> AddWidget(m_buttonLoadGame);
   m_mainMenuLayout -> AddWidget(m_buttonOptions);
   m_mainMenuLayout -> AddWidget(m_buttonExit);
-  m_mainMenuLayout -> AddWidget(m_imageTest);
 
   m_optionsMenuLayout = std::make_shared<GuiLayoutVertical>(m_window);
   m_resolutionInfo = std::make_shared<GuiTextWidget>(m_window);
--- src/gui/include/GuiImage.hpp
+++ src/gui/include/GuiImage.hpp
@@ -23,7 +23,6 @@ public:
   void SetPosition(const float x, const float y) override;
   void SetPosition(const sf::Vector2f &position) override;
 
-
 private:
 
   virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
