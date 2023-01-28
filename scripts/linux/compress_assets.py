7385118533aeee263a58b579ace254694c20bbfa
Jordi2233 <User123.Name456@protonmail.com>
2023-01-28 14:36:42 +0100

    added relatives paths

--- scripts/linux/compress_assets.py
+++ scripts/linux/compress_assets.py
@@ -4,18 +4,22 @@ import shutil
 
 if __name__ == "__main__":
 
-    os.chdir("../..")
+    project_path = os.path.abspath(os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir))
+
+    os.chdir(project_path)
 
     if (os.path.exists("./tools/onyx_lib_compressor.dll") == False):
-      print("[Compress-Assets] Error: onyx_lib_compressor.dll not found")
-      exit()
+        print("[Compress-Assets] Error: onyx_lib_compressor.dll not found")
+        exit()
 
     if (os.path.exists("assets_compressed")):
-      shutil.rmtree("assets_compressed")
+        shutil.rmtree("assets_compressed")
 
     os.mkdir("assets_compressed")
 
-    file_compressor_dll = ctypes.cdll.LoadLibrary("./tools/onyx_lib_compressor.dll")
+    file_compressor_dll = ctypes.cdll.LoadLibrary(
+        "./tools/onyx_lib_compressor.dll")
 
     c_assets_input_dir_wrapper = file_compressor_dll.Py_GetAssetsDirectoryInput
     c_assets_input_dir_wrapper.restype = ctypes.c_char_p
@@ -26,10 +30,7 @@ if __name__ == "__main__":
     c_assets_input_dir = ctypes.c_char_p(c_assets_input_dir_wrapper())
     c_assets_compressor = ctypes.c_void_p(c_assets_compressor_wrapper())
 
-    file_compressor_dll.Py_Compress(c_assets_compressor, c_assets_input_dir, ctypes.c_bool(True))
+    file_compressor_dll.Py_Compress(
+        c_assets_compressor, c_assets_input_dir, ctypes.c_bool(True))
 
     file_compressor_dll.Py_DestroyClass(c_assets_compressor)
-
-
-
-    
